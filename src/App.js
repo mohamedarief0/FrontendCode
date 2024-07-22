b858f701c528f2b767d01c2cde3f345fb8bdd927
Arief <mrarief0110@gmail.com>
2024-07-22 19:00:35 +0530

    fix: minor update on route id

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Registration from "./Auth/Registration";
 import Login from "./Auth/Login";
 import Dashboard from "./Dashboard/Dashboard";
 import MainDashboard from "./MainDashboard/MainDashboard";
+import "../node_modules/material-icons/iconfont/material-icons.css";
 // import "../node_modules/bootstrap/dist/css/bootstrap.css";
 // import "../scss/custom.scss";
 import "./css/main.css"
@@ -18,7 +19,7 @@ function App() {
         {/* <Route path="/" element={<MainDashboard />} /> */}
         <Route path="/api/register" element={<Registration />} />
         <Route path="/api/login" element={<Login setToken={setToken} />} />
-        <Route path="/api/dashboard" element={<MainDashboard  token={token} />} />
+        <Route path="/api/:id" element={<MainDashboard  token={token} />} />
       </Routes>
     </>
   );
